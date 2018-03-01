7e31124d5bbd1094f97b9a9c079ed95262aa8080
leonjorge23 <leonjorge23@gmail.com>
2018-02-28 22:26:45 -0800

    initial commit of a vue, webpack rails app

---
+++ app/javascript/packs/hello_vue.js
@@ -0,0 +1,85 @@
+/* eslint no-console: 0 */
+// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> (and
+// <%= stylesheet_pack_tag 'hello_vue' %> if you have styles in your component)
+// to the head of your layout file,
+// like app/views/layouts/application.html.erb.
+// All it does is render <div>Hello Vue</div> at the bottom of the page.
+
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+import TurbolinksAdapter from 'vue-turbolinks'
+
+function destroyVue(){
+  this.$destroy();
+  document.removeEventListener('turbolinks:before-cache', destroyVue );
+}
+
+document.addEventListener('turbolinks:load', () => {
+  var element = document.getElementById('hello');
+  if (element != null){
+    var app = new Vue({
+      el: element,
+      template: '<App/>',
+      components: { App },
+      beforeMount: function(){
+        this.$originalElment = this.$el.outerHTML;
+        document.addEventListener('turbolinks:before-cache', destroyVue.bind(this));
+      },
+      destroyed: function(){
+        this.$el.outerHTML = this.$originalElment
+      }
+    })
+  }
+})
+
+
+// The above code uses Vue without the compiler, which means you cannot
+// use Vue to target elements in your existing html templates. You would
+// need to always use single file components.
+// To be able to target elements in your existing html/erb templates,
+// comment out the above code and uncomment the below
+// Add <%= javascript_pack_tag 'hello_vue' %> to your layout
+// Then add this markup to your html template:
+//
+// <div id='hello'>
+//   {{message}}
+//   <app></app>
+// </div>
+
+
+// import Vue from 'vue/dist/vue.esm'
+// import App from '../app.vue'
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   const app = new Vue({
+//     el: '#hello',
+//     data: {
+//       message: "Can you say hello?"
+//     },
+//     components: { App }
+//   })
+// })
+//
+//
+//
+// If the using turbolinks, install 'vue-turbolinks':
+//
+// yarn add 'vue-turbolinks'
+//
+// Then uncomment the code block below:
+//
+// import TurbolinksAdapter from 'vue-turbolinks';
+// import Vue from 'vue/dist/vue.esm'
+// import App from '../app.vue'
+//
+// Vue.use(TurbolinksAdapter)
+//
+// document.addEventListener('turbolinks:load', () => {
+//   const app = new Vue({
+//     el: '#hello',
+//     data: {
+//       message: "Can you say hello?"
+//     },
+//     components: { App }
+//   })
+// })
